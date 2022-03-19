d275fa81d9dd53225cdf452c5d654ac6f77b623c
pholim01 <101872228+pholim01@users.noreply.github.com>
2022-03-20 05:37:43 +0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+fetch(
+  "https://developer.spotify.com/dashboard/applications/ae330b8974534b44839052de4b048303",
+  {
+    method: "GET"
+  }
+)
+
+export default App;
